108fbe0aead9884d6a278603609fb371d6c6818c
Mohamed Khalil <71330579+Mohamed-Khalil67@users.noreply.github.com>
2020-11-17 16:47:16 +0100

    Add files via upload

--- my_app.py
+++ my_app.py
@@ -4,14 +4,17 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
+st.header('User Input ')
+st.markdown("CSV input file")
+uploaded_file = st.file_uploader("Upload your input CSV file", type=["csv"])
+if uploaded_file is not None:
+    @st.cache
+    def get_data():
+        return pd.read_csv(uploaded_file)
+    df = get_data()
 
-@st.cache
-def get_data():
-    return pd.read_csv("https://raw.githubusercontent.com/Mohamed-Khalil67/Machine-Learning-Mini-Project/main/diabetes.csv")
 
-df = get_data()
-
-st.title("Streamlit 101: An in-depth introduction")
+st.title("Streamlit Web Application")
 st.markdown("Welcome to this in-depth introduction to how to Use diabetes Dataset")
 st.header("Diabetes Patients Listings: data at a glance")
 
@@ -19,28 +22,43 @@ if st.checkbox("Show DataSet Rows of header"):
     number = st.number_input("Number of Rows to View")
     st.dataframe(df.head(int(number)))
 
-#selec = []
-#for col in df.columns:
-#    selec.append(col)
-#location = set.multiselect("Selectionner les colonnes",(selec))
-#st.table(ds[location].head())
+selec = []
+for col in df.columns:
+   selec.append(col)
+option = st.multiselect("Selectionner les colonnes",(selec))
+st.table(df[option].head())
 
-option = st.multiselect('What Columns to choose?',('Pregnancies', 'Glucose', 'BloodPressure', 'Insulin','BMI','DiabetesPedigreeFunction', 'Age','Outcome'))
+st.header("Discription Choices Boxes")
 
-if st.button("Columns Chosen to Display"):
+if st.button("Dataset Columns Chosen to Display Characteritics"):
     st.table(df[option].head())
-    st.markdown("Afficher le type des colonnes du dataset")
+if st.button("Afficher le type des colonnes du dataset"):
     st.table(df[option].dtypes)
-    st.markdown("La shape du dataset, par lignes et par colonnes")
+if st.button("La shape du dataset, par lignes et par colonnes"):
     st.write(df[option].shape)
-    st.markdown("Afficher les statistiques descriptives du dataset")
+if st.button("Afficher les statistiques descriptives du dataset"):
     st.write(df[option].describe())
 
-if st.button("Show Boxplot"):
-    df.boxplot()
+#graph = []
+#graph = st.multiselect("What kind of graph ?",("Boxplot","Correlation"))
+#st.write("You selected",len(graph),"graph")
+#if 'Boxplot' in graph:
+#    df[option].boxplot()
+#    st.title("Distribution des Variables d'intérêt")
+#    st.pyplot()
+#elif 'Correlation' in graph:
+#    st.write(sns.heatmap(df[option].corr(), annot=True))
+#    st.pyplot()
+
+st.header("What kind of graph ?")
+status = st.radio("Choose ur way of drawing :",('Boxplot','Correlation','Distplot'))
+if status == 'Boxplot':
+    df[option].boxplot()
     st.title("Distribution des Variables d'intérêt")
     st.pyplot()
-
-if st.button("Show Correlation"):
-    st.write(sns.heatmap(df.corr(), annot=True))
+elif status == 'Correlation':
+    st.write(sns.heatmap(df[option].corr(), annot=True))
+    st.pyplot()
+elif status == 'Distplot':
+    st.write(sns.distplot(df[option]))
     st.pyplot()
